1a656305ff1c61cc72336aa23a17b1eb37dcfb09
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-07 17:21:49 +0100

    feat: programma al click su bottone

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
 ("use strict");
 
-// const kmRichiesti = Number(prompt("Quanti Km devi percorrere?"));
-// const etaPasseggiero = Number(prompt("Inesrisci qui la tua età"));
+const bottone1 = document.querySelector("#btn1");
 const prezzoKm = 0.21;
 const percentualeScontoMinorenni = 20;
 const percentualeScontoOver65 = 40;
 
-console.log({ kmRichiesti });
-console.log({ etaPasseggiero });
+bottone1.addEventListener("click", function () {
+  const kmRichiesti = Number(document.querySelector("#km").value);
+  const etaPasseggiero = Number(document.querySelector("#age").value);
+  const elementCostoBiglietto = document.querySelector("#costo-biglietto");
+  console.log(kmRichiesti);
+  console.log(etaPasseggiero);
 
-if (!isNaN(kmRichiesti) && !isNaN(etaPasseggiero)) {
-  let prezzoBiglietto = kmRichiesti * prezzoKm;
-  let scontoBiglietto = 0;
+  if (!isNaN(kmRichiesti) && !isNaN(etaPasseggiero)) {
+    let prezzoBiglietto = kmRichiesti * prezzoKm;
+    let scontoBiglietto = 0;
 
-  if (etaPasseggiero < 18) {
-    scontoBiglietto = (prezzoBiglietto / 100) * percentualeScontoMinorenni;
-  } else if (etaPasseggiero >= 65) {
-    scontoBiglietto = (prezzoBiglietto / 100) * percentualeScontoOver65;
+    if (etaPasseggiero < 18) {
+      scontoBiglietto = (prezzoBiglietto / 100) * percentualeScontoMinorenni;
+    } else if (etaPasseggiero >= 65) {
+      scontoBiglietto = (prezzoBiglietto / 100) * percentualeScontoOver65;
+    }
+
+    prezzoBiglietto -= scontoBiglietto;
+    console.log("Il prezzo del biglietto è: " + prezzoBiglietto.toFixed(2));
+    elementCostoBiglietto.innerHTML = prezzoBiglietto.toFixed(2);
+  } else {
+    console.log("Errore");
   }
-
-  prezzoBiglietto -= scontoBiglietto;
-  console.log("Il prezzo del biglietto è: " + prezzoBiglietto.toFixed(2));
-} else {
-  console.log("Errore");
-}
+});
